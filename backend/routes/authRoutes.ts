116a62912eb4fa61c6711fb423e6aee225b6501d
MKOdeh2024 <mkawadi07@gmail.com>
2025-04-18 20:58:23 +0300

    Updating registration proces with confirm email

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 // backend/routes/authRoutes.js
 import express from 'express';
-import { registerUser, loginUser, getMe } from '../controllers/authController.js';
+import { registerUser, loginUser, getMe, confirmRegistration } from '../controllers/authController.js';
 import { protect } from '../routes/authMiddleware.js';
 // Import validation rules and handler
 import { registerValidationRules, loginValidationRules, handleValidationErrors } from '../validators/authValidator.js';
@@ -14,6 +14,8 @@ router.post(
     handleValidationErrors,   // Handle potential errors
     registerUser             // Proceed to controller if valid
 );
+
+router.get('/confirm/:token', confirmRegistration);
 
 router.post(
     '/login',
